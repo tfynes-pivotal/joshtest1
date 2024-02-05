c95f6048dab4f44638a2349eb5ac995def7d5004
Josh Long <josh@joshlong.com>
2024-02-05 11:52:41 -0800

    let's go

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+
+	@GetMapping("/hello")
+	Map<String, String> hello() {
+		return Map.of("message", "Hello, TAP!");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
